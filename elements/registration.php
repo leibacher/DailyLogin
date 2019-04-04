d8449b1114cf109c18c0d1de875595321ffb956d
leibacher <jan.leibacher@sunrise.ch>
2019-04-04 12:08:45 +0200

    add register style

--- elements/registration.php
+++ elements/registration.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register-submit'])){
-    
+    $name = $_POST['username'];
     $error = "";
     if(!isset($_POST['username'])){
        $error = "Kein Benutzername gesetzt. ";
@@ -10,6 +10,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register-submit'])){
         $letters = strlen($_POST['username']);
         if($letters > 45 OR $letters < 3){
             $error = $error."Username zu lang oder zu kurz. Maximal 45 Zeichen, minimal 3 Zeichen.";
+        }
+    }
+/* Check if the username is already taken */
+    $sql = "SELECT name FROM user where name='$name';";
+    $result = Database::getData($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if($row["name"]==$name){
+                $error = $error."Benutzername bereits vergeben";
+            }
         }
     }
    
@@ -44,21 +54,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register-submit'])){
     if(!isset($_POST['password_confirmed'])){
        $error = $error."Keine Passwort Bestätigung gesetzt. ";
     }
-    $name = $_POST['username'];
+    
     $password = sha1($_POST['password']);
     $password_confirmed = sha1($_POST['password_confirmed']);
     $lastonline = new DateTime();
      
-/* Check if the username is already taken */
-    $sql = "SELECT name FROM user where name='$name';";
-    $result = Database::getData($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            if($row["name"]==$name){
-                $error = $error."Benutzername bereits vergeben";
-            }
-        }
-    }
+
     
 /* checks if both password entries are identical */
     if($password!=$password_confirmed){
@@ -83,24 +84,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register-submit'])){
         <?php
     if(isset($_POST['username']) AND $error!=""){ ?>
 
-            <p style="color:red;">
+            <p style="font-size:12px; text-aligne:left; color:red;">
                 <?php echo $error; ?>
             </p>
 
             <form id='register' action='?p=register' method='post' accept-charset='UTF-8' enctype="multipart/form-data" autocomplete="on">
                 <div class="register-form">
-                    <label for='username'>Benutzername*:</label>
+                    <label class="label_username" for='username'>Benutzername*:</label>
                     <input type='text' name='username' id='username' value="<?php echo $_POST['username']; ?>" maxlength="45" autocomplete="username" required/>
                     <p id="username-alert" class="register-alert"></p>
                 </div>
                 <div class="register-form">
-                    <label for='password'>Passwort*:</label>
-                    <input type='password' name='password' id='password' maxlength="45" autocomplete="new-password" required/>
-                    <p id="password-alert" class="register-alert"></p>
+                    <label class="label_password" for='password'>Passwort*:</label>
+                    <input class="input_password" type='password' name='password' id='password' maxlength="45" autocomplete="new-password" required/>
+                
                 </div>
                 <div class="register-form">
-                    <label for='password'>Passwort bestätigen*:</label>
-                    <input type='password' name='password_confirmed' id='password_confirmed' maxlength="45" required/>
+                    <label class="label_password" for='password'>Passwort bestätigen*:</label>
+                    <input class="input_password" type='password' name='password_confirmed' id='password_confirmed' maxlength="45" required/>
                     <p id="password-confirm-alert" class="register-alert"></p>
                 </div>
                 <div class="register-form">
@@ -116,18 +117,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register-submit'])){
 
             <form id='register' action='?p=register' method='post' accept-charset='UTF-8' enctype="multipart/form-data" autocomplete="on">
                 <div class="register-form">
-                    <label for='username'>Benutzername*:</label>
+                    <label class="label_username" for='username'>Benutzername*:</label>
                     <input type='text' value="" name='username' id='username' maxlength="45" autocomplete="username" required/>
                     <p id="username-alert" class="register-alert"></p>
                 </div>
                 <div class="register-form">
-                    <label for='password'>Passwort*:</label>
-                    <input type='password' value="" name='password' id='password' class="firstpw" maxlength="45" autocomplete="new-password" required/>
-                    <p id="password-alert" class="register-alert"></p>
+                    <label class="label_password" for='password'>Passwort*:</label>
+                    <input class="input_password" type='password' value="" name='password' id='password' class="firstpw" maxlength="45" autocomplete="new-password" required/>
+             
                 </div>
                 <div class="register-form">
-                    <label for='password'>Passwort bestätigen*:</label>
-                    <input type='password' value="" name='password_confirmed' id='password_confirmed' maxlength="45" required/>
+                    <label class="label_password" for='password'>Passwort bestätigen*:</label>
+                    <input class="input_password" type='password' value="" name='password_confirmed' id='password_confirmed' maxlength="45" required/>
                     <p id="password-confirm-alert" class="register-alert"></p>
                 </div>
                 <div class="register-form">
@@ -154,13 +155,18 @@ jQuery("#register").on("blur", "input[name=username]", function() {
             setTimeout(function() { 
                 $("#username-alert").text("");
                 if (data != "") {
-
+                    $("#username-alert").css("display", "block");
                     $("#username-alert").append(data);
                     $("#username-alert").css("color", "red");
+                    $("#username").css("border", "red solid 2px");
+                    $(".label_username").css("color", "red");  
                     checkusername = false;
                 } else {
+                    $("#username-alert").css("display", "block");
                     $("#username-alert").append("Benutzername noch verfügbar");
                     $("#username-alert").css("color", "green");
+                    $("#username").css("border", "green solid 2px");
+                    $(".label_username").css("color", "green");  
                     checkusername = true;
                 }
             }, 2000); 
@@ -179,13 +185,19 @@ jQuery("#register").on("keyup", "input[name=password]", function() {
     var pw = jQuery(this).val();
     var pw_confirmed = jQuery("input[name=password_confirmed]").val(); 
     if(pw==pw_confirmed){
+        $("#password-confirm-alert").css("display", "block");
         $("#password-confirm-alert").text("");
         $("#password-confirm-alert").append("Passwörter sind gleich.");
-        $("#password-confirm-alert").css("color", "green");   
+        $("#password-confirm-alert").css("color", "green");
+        $(".input_password").css("border", "green solid 2px");
+        $(".label_password").css("color", "green");  
     }else{
+        $("#password-confirm-alert").css("display", "block");
         $("#password-confirm-alert").text("");
         $("#password-confirm-alert").append("Passwörter sind nicht gleich.");
-        $("#password-confirm-alert").css("color", "red");   
+        $("#password-confirm-alert").css("color", "red");
+        $(".input_password").css("border", "red solid 2px");
+        $(".label_password").css("color", "red");  
     }
 });
 /* checks if both password entries are identical while typing the second password*/  
@@ -193,13 +205,19 @@ jQuery("#register").on("keyup", "input[name=password_confirmed]", function() {
     var pw = jQuery(this).val();
     var pw_confirmed = jQuery("input[name=password]").val(); 
     if(pw==pw_confirmed){
+        $("#password-confirm-alert").css("display", "block");
         $("#password-confirm-alert").text("");
         $("#password-confirm-alert").append("Passwörter sind gleich.");
-        $("#password-confirm-alert").css("color", "green");   
+        $("#password-confirm-alert").css("color", "green");
+        $(".input_password").css("border", "green solid 2px");
+        $(".label_password").css("color", "green");  
     }else{
+        $("#password-confirm-alert").css("display", "block");
         $("#password-confirm-alert").text("");
         $("#password-confirm-alert").append("Passwörter sind nicht gleich.");
-        $("#password-confirm-alert").css("color", "red");   
+        $("#password-confirm-alert").css("color", "red");
+        $(".input_password").css("border", "red solid 2px");
+        $(".label_password").css("color", "red");  
     }
 });   
 </script>
